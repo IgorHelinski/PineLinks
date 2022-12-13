0d178435995b6d29a59b0809b4ed0f22f082e103
IgorHelinski <igor.www.pl.com@gmail.com>
2022-12-13 21:03:49 +0100

    Added logging system

--- PineLinks/Controllers/LoginController.cs
+++ PineLinks/Controllers/LoginController.cs
@@ -1,12 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Security.Claims;
+using PineLinks.Models;
 
 namespace PineLinks.Controllers
 {
     public class LoginController : Controller
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        private IConfiguration Configuration;
+        public LoginController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = this.Configuration.GetConnectionString("ConString");
+        }
+
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Login(LoginModel log, UserModel usr)
+        {
+            
+            //sprawdza czy w bazie danych jest taki urzytkownik
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from dbo.Users where UserLogin='" + log.Login + "' and UserPassword='" + log.Password + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                usr.Login = dr["UserLogin"].ToString();
+                usr.Password = dr["UserPassword"].ToString();
+                usr.Name = dr["UserName"].ToString();
+                usr.Email = dr["UserEmail"].ToString();
+                usr.Role = dr["UserRole"].ToString();
+
+                //znalazl urzytkownika
+
+                //log.loggedIn = true;
+                //usr.loggedIn = true;
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email,usr.Email),
+                    new Claim(ClaimTypes.Name,usr.Name),
+                    new Claim(ClaimTypes.Role,usr.Role),
+                    new Claim(ClaimTypes.SerialNumber, dr["UserId"].ToString())
+
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new System.Security.Claims.ClaimsPrincipal(claimsIdentity));
+
+               
+
+                con.Close();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                //nie znalazl urzytkownika
+                con.Close();
+                //log.loggedIn = false;
+                ViewData["LoginFlag"] = "Nieprawidłowy login lub hasło!!!";
+                return View("Index");
+            }
+        }
+        public IActionResult Logout() //Wylogowanie
+        {
+            Response.Cookies.Delete("sesja");
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ PineLinks/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PineLinks.Models
+{
+    public class UserModel
+    {
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
