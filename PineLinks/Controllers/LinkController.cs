72e36e5b3a51bb1fad4e609934651aaa8ad4b5c6
IgorHelinski <igor.www.pl.com@gmail.com>
2022-12-15 20:12:16 +0100

    Added adding and deleting links

--- PineLinks/Controllers/LinkController.cs
+++ PineLinks/Controllers/LinkController.cs
@@ -1,15 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using PineLinks.Models;
+using System.Data;
 
 namespace PineLinks.Controllers
 {
     public class LinkController : Controller
     {
-        public IActionResult DeleteLink()
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        private IConfiguration Configuration;
+        public LinkController(IConfiguration _configuration)
         {
-            return View();
+            Configuration = _configuration;
         }
 
-        public IActionResult EditLink()
+        void connectionString()
+        {
+            con.ConnectionString = this.Configuration.GetConnectionString("ConString");
+        }
+        public IActionResult DeleteLink(string id)
+        {
+            string[] idSplit;
+            idSplit = id.Split("!");
+            string ownerName = idSplit[0];
+            string linkId = idSplit[1];
+
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Delete from dbo.Links where OwnerName='" + ownerName + "' and LinkId='" + linkId + "'";
+            dr = com.ExecuteReader();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult EditLink(string id)
         {
             return View();
         }
@@ -17,6 +45,20 @@ namespace PineLinks.Controllers
         public IActionResult AddLink()
         {
             return View();
+        }
+
+        public IActionResult Add(LinkModel link)
+        {
+            connectionString();
+            SqlCommand com = new SqlCommand("usr_AddLink", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@OwnerName", User.Identity.Name.ToString());
+            com.Parameters.AddWithValue("@LinkLabel", link.LinkLabel);
+            com.Parameters.AddWithValue("@LinkUrl", link.LinkUrl);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
