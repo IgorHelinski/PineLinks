b1b37a9a6fc8f64e0d06daaf394780d60c81a9d9
IgorHelinski <igor.www.pl.com@gmail.com>
2022-12-17 18:11:16 +0100

    Bug fixes and user profiles on home page

--- PineLinks/Controllers/HomeController.cs
+++ PineLinks/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PineLinks.Models;
 using System.Diagnostics;
+using System.Data.SqlClient;
 
 namespace PineLinks.Controllers
 {
@@ -9,14 +10,53 @@ namespace PineLinks.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public static IList<UserModel> Users = new List<UserModel>();
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        private IConfiguration Configuration;
+        public HomeController(IConfiguration _configuration, ILogger<HomeController> logger)
         {
+            Configuration = _configuration;
             _logger = logger;
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = this.Configuration.GetConnectionString("ConString");
         }
 
         public IActionResult Index()
         {
-            return View();
+            Users.Clear();
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from dbo.Users";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                Users.Add(new UserModel
+                {
+                    Name = dr["UserName"].ToString()
+                });
+
+                while (dr.Read())
+                {
+                    Users.Add(new UserModel
+                    {
+                        Name = dr["UserName"].ToString()
+                    });
+                }
+
+                return View(Users);
+            }
+            else
+            {
+                return View(Users);
+            }
         }
 
         [Authorize]
